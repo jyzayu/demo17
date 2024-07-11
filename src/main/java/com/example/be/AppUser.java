c60707ee6d0bac497f10d553413b3254d512c733
jyzayu <moosou30@naver.com>
2024-07-11 16:26:26 +0900

    demo

--- src/main/java/com/example/be/AppUser.java
+++ src/main/java/com/example/be/AppUser.java
@@ -1,2 +1,20 @@
-package com.example.be;public class AppUser {
+package com.example.be;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+    public AppUser(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/be/DemoController.java
+++ src/main/java/com/example/be/DemoController.java
@@ -1,2 +1,24 @@
-package com.example.be;public class demoController {
+package com.example.be;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+
+    private final DemoRepository demoRepository;
+
+
+    @PostMapping("/user")
+    public void saveUsers() {
+        demoRepository.save(new AppUser("b"));
+    }
+
+    @GetMapping("/userInfo")
+    public String getUserName() {
+        return demoRepository.findAll().get(1  ).getClass().getName();
+    }
 }
